dc92b8ae0652e6ce62deb11ddc8f1f198bf94417
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-11-12 14:50:31 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,116 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  // let studentName = "Ömer Faruk Çelik";
+  // const [studentName, setStudentName] = useState("Ömer Faruk Çelik");
+  const [studentName, setStudentName] = useState("");
+  const [studentCourseName, setStudentCourseName] = useState("");
+  const [studentInstructorName, setStudentInstructorName] = useState("");
+  const [studentScore, setStudentScore] = useState("");
+
+  const [student, setStudent] = useState({});
+
+  const [studentNameError, setStudentNameError] = useState(false);
+  const [studentCourseNameError, setStudentCourseNameError] = useState(false);
+  const [studentInstructorNameError, setStudentInstructorNameError] = useState(false);
+  const [studentScoreError, setStudentScoreError] = useState(false);
+  const addStudent = (event) => {
+    event.preventDefault();
+
+    setStudentNameError(false);
+    setStudentCourseNameError(false);
+    setStudentInstructorNameError(false);
+    setStudentScoreError(false);
+
+    if (studentName && studentCourseName && studentInstructorName && studentScore) {
+
+      setStudent({
+        name: studentName,
+        courseName: studentCourseName,
+        instructorName: studentInstructorName,
+        score: studentScore
+      });
+
+      setStudentName("");
+      setStudentCourseName("");
+      setStudentInstructorName("");
+      setStudentScore("");
+    } else {
+      !studentName && setStudentNameError(true);
+      !studentCourseName && setStudentCourseNameError(true);
+      !studentInstructorName && setStudentInstructorNameError(true);
+      !studentScore && setStudentScoreError(true);
+    }
+  }
+
+
+  // const setAndUpdateName = studentName => {
+  //   if(!studentName){
+  //     throw new Error("Hata mesajı");
+  //   }
+
+  //   if (!(studentName.length > 2)) {
+  //     throw new Error();
+  //   }
+
+
+
+  //   setUpdatedStudentName(studentName.trim());
+  // }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1 className='app-header'>Student Manager</h1>
+      <h2>{studentName}</h2>
+      <div className='student'>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Course Name</th>
+              <th>Instructor Name</th>
+              <th>Score</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{student.name}</td>
+              <td>{student.courseName}</td>
+              <td>{student.instructorName}</td>
+              <td>{student.score}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <form>
+        <div className='form-group'>
+          <label>Name</label>
+          <input type="text" value={studentName} minLength={2} placeholder='Enter Student Name' onChange={event => setStudentName(event.target.value)} />
+          {studentNameError && <span>Geçerli bir değer giriniz!</span>}
+        </div>
+        <div className='form-group'>
+          <label>Course Name</label>
+          <input type="text" value={studentCourseName} placeholder='Enter Student Course Name' onChange={event => setStudentCourseName(event.target.value)} />
+          {studentCourseNameError && <span>Geçerli bir değer giriniz!</span>}
+        </div>
+        <div className='form-group'>
+          <label>Instructor Name</label>
+          <input type="text" value={studentInstructorName} placeholder='Enter Student Instructor Name' onChange={event => setStudentInstructorName(event.target.value)} />
+          {studentInstructorNameError && <span>Geçerli bir değer giriniz!</span>}
+        </div>
+        <div className='form-group'>
+          <label>Score</label>
+          <input type="number" value={studentScore} placeholder='Enter Student Score' onChange={event => setStudentScore(event.target.value)} />
+          {studentScoreError && <span>Geçerli bir değer giriniz!</span>}
+        </div>
+        <div className='form-group'>
+          <button className='btn-primary' onClick={addStudent}>
+            Add Student
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
