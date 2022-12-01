750f0ba41573e4f6c8ccdc431aa6d43acc49b411
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-12-01 22:39:16 +0300

    search added

--- src/components/app/student/list/List.js
+++ src/components/app/student/list/List.js
@@ -1,19 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
 const List = ({ students }) => {
+  const [filteredStudentList, setFilteredStudentList] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const search = (event) => {
+    setSearchTerm(event.target.value);
+    const normalizedSearchTerm = event.target.value.trim().toLowerCase();
+    if (normalizedSearchTerm) {
+      setFilteredStudentList(
+        students.filter((student) =>
+          Object.values(student).some((val) =>
+            val.toLowerCase().includes(normalizedSearchTerm)
+          )
+        )
+      );
+    } else {
+      setFilteredStudentList([]);
+    }
+  };
+
   return (
     <>
       <div className="student">
         <table>
           <thead>
             <tr>
-              <th colSpan={4} >Student List</th>
+              <th colSpan={4}>Student List</th>
             </tr>
             <tr>
               <th colSpan={2}></th>
-              <th style={{textAlign:"end"}}>Search :</th>
+              <th style={{ textAlign: "end" }}>Search :</th>
               <th>
-                <input style={{width:"90%"}} type="text" onChange={event => console.log(event.target.value)} />
+                <input
+                  style={{ width: "90%" }}
+                  type={"search"}
+                  onChange={search}
+                />
               </th>
             </tr>
             <tr>
@@ -24,14 +47,25 @@ const List = ({ students }) => {
             </tr>
           </thead>
           <tbody>
-            {students.map(({ name, course, instructor, score }, index) => (
-              <tr key={index}>
-                <td>{name}</td>
-                <td>{course}</td>
-                <td>{instructor}</td>
-                <td>{score}</td>
-              </tr>
-            ))}
+            {filteredStudentList.length
+              ? filteredStudentList.map(
+                  ({ name, course, instructor, score }, index) => (
+                    <tr key={index}>
+                      <td>{name}</td>
+                      <td>{course}</td>
+                      <td>{instructor}</td>
+                      <td>{score}</td>
+                    </tr>
+                  )
+                )
+              : students.map(({ name, course, instructor, score }, index) => (
+                  <tr key={index}>
+                    <td>{name}</td>
+                    <td>{course}</td>
+                    <td>{instructor}</td>
+                    <td>{score}</td>
+                  </tr>
+                ))}
           </tbody>
         </table>
       </div>
